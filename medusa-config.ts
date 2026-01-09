ee5b9345dfee3a5afed8bbfc8370bc5e25e5e5d8
Daniel Caicedo <79131464+danielcaicedo95@users.noreply.github.com>
2026-01-09 09:18:02 -0500

    4 cambio

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,21 +1,27 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig } from "@medusajs/framework/utils"
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || "development", process.cwd())
 
 export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+
+    redisUrl: process.env.REDIS_URL,
+
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
   },
+
   admin: {
     disable: false,
+    backendUrl: process.env.MEDUSA_ADMIN_BACKEND_URL,
   },
+
   modules: [
     {
       resolve: "@medusajs/medusa/payment",
@@ -34,5 +40,5 @@ export default defineConfig({
         ],
       },
     },
-  ]
+  ],
 })
