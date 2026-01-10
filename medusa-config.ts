38ee08f1555c42d43d022704554df065ddaccc2d
Daniel Caicedo <79131464+danielcaicedo95@users.noreply.github.com>
2026-01-10 17:21:07 -0500

    Estandarización final a ESM y TypeScript (Medusa v2 nativo)

---
+++ medusa-config.ts
@@ -0,0 +1,43 @@
+import { loadEnv, defineConfig } from "@medusajs/framework/utils"
+
+loadEnv(process.env.NODE_ENV || "development", process.cwd())
+
+export default defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
+    http: {
+      storeCors: process.env.STORE_CORS,
+      adminCors: process.env.ADMIN_CORS,
+      authCors: process.env.AUTH_CORS,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    },
+  },
+  admin: {
+    disable: false,
+    outDir: "build-admin",
+    ...(process.env.MEDUSA_ADMIN_BACKEND_URL && {
+      backendUrl: process.env.MEDUSA_ADMIN_BACKEND_URL,
+    }),
+  },
+  modules: [
+    {
+      resolve: "@medusajs/medusa/payment",
+      options: {
+        providers: [
+          {
+            resolve: "./src/modules/wompi-payment",
+            id: "wompi",
+            options: {
+              publicKey: process.env.WOMPI_PUB_KEY,
+              privateKey: process.env.WOMPI_PRV_KEY,
+              integritySecret: process.env.WOMPI_INTEGRITY_SECRET,
+              testMode: process.env.WOMPI_TEST_MODE === "true",
+            },
+          },
+        ],
+      },
+    },
+  ],
+})
