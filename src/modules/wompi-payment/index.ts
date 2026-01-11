d421f186a0414328079c121f78843b239386e325
Daniel Caicedo <79131464+danielcaicedo95@users.noreply.github.com>
2026-01-10 19:45:21 -0500

    Remove .js extensions from imports

--- src/modules/wompi-payment/index.ts
+++ src/modules/wompi-payment/index.ts
@@ -1,5 +1,5 @@
 import { ModuleProvider, Modules } from "@medusajs/framework/utils"
-import WompiPaymentProviderService from "./service.js"
+import WompiPaymentProviderService from "./service"
 
 export default ModuleProvider(Modules.PAYMENT, {
     services: [WompiPaymentProviderService],
